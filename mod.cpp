d893f0daef89ab4c4e83741bb6208917524decb8
ConnorAU <15099385+ConnorAU@users.noreply.github.com>
2025-04-29 08:03:15 +1000

    Add mod.cpp Adding to git for use in remote build workflow

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name="Extended Chat";
+overview="Adds new functionality to the chat system with emojis, history viewer, message filters and commands!";
+tooltipOwned="A3ExtendedChat";
+action="https://github.com/ConnorAU/A3ExtendedChat";
+
+hideName=0;
+hidePicture=0;
